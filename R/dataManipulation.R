a604aacd4eb41a18a07b12a325872b3659425905
PJ <52793278+pieterjanvc@users.noreply.github.com>
2025-10-31 18:56:18 -0400

    Fix bug in UPDATE statement creation (#3)

--- R/dataManipulation.R
+++ R/dataManipulation.R
@@ -109,7 +109,7 @@ tbl_update <- function(dataframe, dbInfo, table, commit = T, constraints = T) {
         sprintf(
           'UPDATE "%s" SET %s WHERE %s RETURNING "%s"',
           table,
-          paste(sprintf('"%s" = ?', toUpdate), collapse = " AND "),
+          paste(sprintf('"%s" = ?', toUpdate), collapse = ", "),
           paste(sprintf('"%s" = ?', check$pk), collapse = " AND "),
           paste(originalOrder, collapse = '","')
         ),
